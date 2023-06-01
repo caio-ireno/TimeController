3f8d416b35db6c176abdf304ce8a4c8f6151c8d9
caio-ireno <caio_ireno@hotmail.com>
2023-05-31 21:27:21 -0300

    Navigation

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { House } from 'phosphor-react-native'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
