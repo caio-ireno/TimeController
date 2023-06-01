24c3c3b97b6a190bef0c2bb6876fd064b7932f9e
caio-ireno <caio_ireno@hotmail.com>
2023-06-01 12:30:31 -0300

    handle gesture

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
+import { StatusBar } from 'react-native'
+
 import { AppThemeProvider } from './src/context/ThemeContext'
 import { Routes } from './src/routes'
 
 function App() {
   return (
     <AppThemeProvider>
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="dark-content"
+        translucent
+      />
       <Routes></Routes>
     </AppThemeProvider>
   )
