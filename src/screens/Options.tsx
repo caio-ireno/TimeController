6f67640d0fe4b64a9cf809d5f019d65d71c64b41
caio-ireno <caio_ireno@hotmail.com>
2023-06-02 13:19:46 -0300

    style button

--- src/screens/Options.tsx
+++ src/screens/Options.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import Button from '../components/Button'
 import { PageDefault } from '../components/PageDefault'
 import { useAppThemeContext } from '../context/ThemeContext'
 import { styles } from '../style/style'
+import { Moon, Sun } from 'phosphor-react-native'
 
 export function Options() {
+  const [themeChange, setThemeChange] = useState(false)
   const { theme, toggleTheme } = useAppThemeContext()
   const dynamicStyles = styles(theme)
 
   return (
     <PageDefault>
       <View style={dynamicStyles.container}>
-        <View
+        <TouchableOpacity
+          onPress={() => {
+            toggleTheme(), setThemeChange(!themeChange)
+          }}
           style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
-            width: '90%',
+            width: '95%',
+            height: 50,
+            borderRadius: 10,
+            alignItems: 'center',
+            paddingHorizontal: 10,
+            backgroundColor: theme.buttonBackground,
           }}
         >
-          <Text style={{ fontSize: 20, color: theme.textColor }}>Tema</Text>
-          <Button label="Tema" fontSize={20} onPress={toggleTheme} />
-        </View>
+          <Text style={{ fontSize: 20, color: theme.buttonText }}>
+            Mudar o tema
+          </Text>
+          {themeChange && <Sun size={30} />}
+          {!themeChange && <Moon size={30} />}
+        </TouchableOpacity>
       </View>
     </PageDefault>
   )
