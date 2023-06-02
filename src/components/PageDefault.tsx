a5bb7efeeb5ef4819833efac2261965db9df053e
caio-ireno <caio_ireno@hotmail.com>
2023-06-02 12:47:59 -0300

    keyBoard on

--- src/components/PageDefault.tsx
+++ src/components/PageDefault.tsx
@@ -1,3 +1,4 @@
+import { useKeyboard } from '@react-native-community/hooks'
 import { useNavigation } from '@react-navigation/native'
 import { Gear, House } from 'phosphor-react-native'
 import { TouchableOpacity, View } from 'react-native'
@@ -11,6 +12,8 @@ interface AppThemeProviderProps {
 export const PageDefault: React.FC<AppThemeProviderProps> = ({ children }) => {
   const navigation = useNavigation()
   const { theme } = useAppThemeContext()
+  const keyboard = useKeyboard()
+  //const [onKeyBoard, setOnKeyBoard] = useState(false)
 
   const handleHome = () => {
     navigation.navigate('home')
@@ -19,25 +22,29 @@ export const PageDefault: React.FC<AppThemeProviderProps> = ({ children }) => {
   const handleOption = () => {
     navigation.navigate('option')
   }
+
   return (
     <View style={{ flex: 1 }}>
       {children}
-      <View
-        style={{
-          height: 60,
-          backgroundColor: theme.buttonBackground,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}
-      >
-        <TouchableOpacity onPress={handleHome}>
-          <House size={30} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleOption}>
-          <Gear size={30} />
-        </TouchableOpacity>
-      </View>
+
+      {!keyboard.keyboardShown && (
+        <View
+          style={{
+            height: 60,
+            backgroundColor: theme.buttonBackground,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+          }}
+        >
+          <TouchableOpacity onPress={handleHome}>
+            <House size={30} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleOption}>
+            <Gear size={30} />
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   )
 }
